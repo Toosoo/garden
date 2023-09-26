d1827a2a12b3c374eceeaeb032cc59ce7fe87cbd
Toosoo <hello@iamtoso.com>
2023-09-26 15:20:59 +0300

    text

--- src/app/Models/Insta.jsx
+++ src/app/Models/Insta.jsx
@@ -1,15 +1,23 @@
-import { Float, useGLTF, useMatcapTexture } from "@react-three/drei";
+import { Float, Html, useGLTF, useMatcapTexture } from "@react-three/drei";
 
 export function Insta(props) {
   const { nodes, materials } = useGLTF("/glbs/insta.glb");
   const [basicMatcap] = useMatcapTexture("5ECBA4_BCFAE7_92EED0_A1F4DC", 64);
   return (
     <Float  speed={3} >
+      
       <group {...props} dispose={null} scale={0.03}>
         <mesh geometry={nodes.Instagram_2_Instagram1_0.geometry} >
         <meshMatcapMaterial matcap={basicMatcap} />
             </mesh>
       </group>
+
+      <Html>
+        
+      </Html>
+
+
+      
     </Float>
   );
 }
--- src/app/Text/Text.jsx
+++ src/app/Text/Text.jsx
@@ -4,12 +4,13 @@ import { useEffect, useRef } from "react";
 
 export default function Text() {
   const [glassMatCap, url] = useMatcapTexture("2E763A_78A0B7_B3D1CF_14F209", 64);
+  const [basicMatcap] = useMatcapTexture("5ECBA4_BCFAE7_92EED0_A1F4DC", 64);
 
   const fontProps = {
-    font: "/fonts/Oswald_Regular.json",
+    font: "/fonts/SC-font.json",
     bevelEnabled: true,
     bevelSize: 0,
-    bevelThickness: 0.05,
+    bevelThickness: 0.03,
     curveSegments: 10,
     bevelOffset: 0,
     bevelSegments: 1,
@@ -20,46 +21,74 @@ export default function Text() {
 
   return (
     <group position={[0, 0, 3.8]}>
+       <RigidBody position={[-0.6, 0, 0]} rotation-y={0.2}>
+        <Text3D {...fontProps}>
+          C
+          <meshMatcapMaterial matcap={basicMatcap} />
+        </Text3D>
+      </RigidBody>
+
       <RigidBody position={[-0.5, 0, 0]} rotation-y={0.2}>
         <Text3D {...fontProps}>
+          R
+          <meshMatcapMaterial matcap={basicMatcap} />
+        </Text3D>
+      </RigidBody>
+
+      <RigidBody position={[-0.4, 0, 0]} rotation-y={-0.1}>
+        <Text3D {...fontProps}>
+          e
+          <meshMatcapMaterial matcap={basicMatcap} />
+        </Text3D>
+      </RigidBody>
+
+      <RigidBody position={[-0.3, 0, 0]} rotation-y={0}>
+        <Text3D {...fontProps}>
+          a
+          <meshMatcapMaterial matcap={basicMatcap} />
+        </Text3D>
+      </RigidBody> 
+
+      <RigidBody position={[-0.2, 0, 0]} rotation-y={0}>
+        <Text3D {...fontProps}>
           T
-          <meshMatcapMaterial matcap={glassMatCap} />
+          <meshMatcapMaterial matcap={basicMatcap} />
         </Text3D>
-      </RigidBody>
+      </RigidBody> 
 
-      <RigidBody position={[-0.3, 0, 0]} rotation-y={0.2}>
+      <RigidBody position={[0.1, 0, 0]} rotation-y={0}>
         <Text3D {...fontProps}>
-          o
-          <meshMatcapMaterial matcap={glassMatCap} />
+          i
+          <meshMatcapMaterial matcap={basicMatcap} />
         </Text3D>
-      </RigidBody>
+      </RigidBody> 
+      <RigidBody position={[0.2, 0, 0]} rotation-y={0}>
+        <Text3D {...fontProps}>
+          V
+          <meshMatcapMaterial matcap={basicMatcap} />
+        </Text3D>
+      </RigidBody> 
+      <RigidBody position={[0.3, 0, 0]} rotation-y={0}>
+        <Text3D {...fontProps}>
+          e
+          <meshMatcapMaterial matcap={basicMatcap} />
+        </Text3D>
+      </RigidBody> 
 
-      <RigidBody position={[-0.05, 0, 0]} rotation-y={-0.1}>
-        <Text3D {...fontProps}>
-          S
-          <meshMatcapMaterial matcap={glassMatCap} />
-        </Text3D>
-      </RigidBody>
 
-      <RigidBody position={[0.25, 0, 0]} rotation-y={0}>
-        <Text3D {...fontProps}>
-          o
-          <meshMatcapMaterial matcap={glassMatCap} />
-        </Text3D>
-      </RigidBody>
+
+
+     
 
       <RigidBody position={[0.2, 0.6, .05]} rotation={[0, 0, -0.3]} gravityScale={0.04}>
-        <Text3D {...fontProps} size={0.1} bevelThickness={0.01}>
-          CREATIVE DEV
-          <meshMatcapMaterial matcap={glassMatCap} />
+        <Text3D {...fontProps} size={0.4} bevelThickness={0.03}>
+          Dev
+          <meshMatcapMaterial matcap={basicMatcap} />
         </Text3D>
-      </RigidBody>
-      <RigidBody position={[-0.4, 0.5, .02]} rotation={[0, 0, 0]} gravityScale={0.01}>
-        <Text3D {...fontProps} size={0.07} bevelThickness={0.01}>
-          FREELANCE
-          <meshMatcapMaterial matcap={glassMatCap} />
-        </Text3D>
-      </RigidBody>
+      </RigidBody> 
+
+     
+
     </group>
   );
 }
